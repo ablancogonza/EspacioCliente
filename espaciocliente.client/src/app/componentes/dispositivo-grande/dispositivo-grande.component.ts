26b033974317ed031bbf1d6be5e4386130f9543d
abg <softabg@gmail.com>
2024-10-20 13:52:09 +0200

    Activa componentes dependiendo de la vista seleccionada

--- espaciocliente.client/src/app/componentes/dispositivo-grande/dispositivo-grande.component.ts
+++ espaciocliente.client/src/app/componentes/dispositivo-grande/dispositivo-grande.component.ts
@@ -4,14 +4,24 @@ import { SplitterModule } from 'primeng/splitter';
 import { CabeceraComponent } from '../cabecera/cabecera.component';
 import { ArbolComponent } from '../arbol/arbol.component';
 import { SelectorVistaComponent } from '../selector-vista/selector-vista.component';
+import { EstadoService } from '../../servicios/estado.service';
+import { VistaSeleccionada } from '../../enumerados/vista-seleccionada';
+import { Observable } from 'rxjs';
+import { ContenedorGraficosComponent } from '../contenedor-graficos/contenedor-graficos.component';
+import { MapaComponent } from '../mapa/mapa.component';
+import { ContenedorConversacionesComponent } from '../contenedor-conversaciones/contenedor-conversaciones.component';
 
 @Component({
   selector: 'app-dispositivo-grande',
   standalone: true,
-  imports: [CommonModule, SplitterModule, CabeceraComponent, ArbolComponent, SelectorVistaComponent],
+  imports: [CommonModule, SplitterModule, CabeceraComponent, ArbolComponent, SelectorVistaComponent, ContenedorGraficosComponent, MapaComponent, ContenedorConversacionesComponent],
   templateUrl: './dispositivo-grande.component.html',
   styleUrl: './dispositivo-grande.component.css'
 })
 export class DispositivoGrandeComponent {
+  vista$: Observable<VistaSeleccionada>;
+  constructor(private estadoService: EstadoService) {
+    this.vista$ = estadoService.selectorVista.vista$;
+  }
 
 }
--- espaciocliente.client/src/app/componentes/dispositivo-pequenio/dispositivo-pequenio.component.ts
+++ espaciocliente.client/src/app/componentes/dispositivo-pequenio/dispositivo-pequenio.component.ts
@@ -3,14 +3,24 @@ import { Component } from '@angular/core';
 import { CabeceraComponent } from '../cabecera/cabecera.component';
 import { ArbolComponent } from '../arbol/arbol.component';
 import { SelectorVistaComponent } from '../selector-vista/selector-vista.component';
+import { MapaComponent } from '../mapa/mapa.component';
+import { ContenedorGraficosComponent } from '../contenedor-graficos/contenedor-graficos.component';
+import { ContenedorConversacionesComponent } from '../contenedor-conversaciones/contenedor-conversaciones.component';
+import { Observable } from 'rxjs';
+import { VistaSeleccionada } from '../../enumerados/vista-seleccionada';
+import { EstadoService } from '../../servicios/estado.service';
 
 @Component({
   selector: 'app-dispositivo-pequenio',
   standalone: true,
-  imports: [CommonModule, CabeceraComponent, ArbolComponent, SelectorVistaComponent],
+  imports: [CommonModule, CabeceraComponent, ArbolComponent, SelectorVistaComponent, ArbolComponent, MapaComponent, ContenedorGraficosComponent, ContenedorConversacionesComponent],
   templateUrl: './dispositivo-pequenio.component.html',
   styleUrl: './dispositivo-pequenio.component.css'
 })
 export class DispositivoPequenioComponent {
+  vista$: Observable<VistaSeleccionada>;
 
+  constructor(private estadoService: EstadoService) {
+    this.vista$ = estadoService.selectorVista.vista$;
+  }
 }
